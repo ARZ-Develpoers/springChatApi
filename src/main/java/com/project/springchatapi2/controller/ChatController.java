ae88ca1eb9954c5c969730e7406328e66150f834
sym <sym@humuson.com>
2022-03-30 23:28:55 +0900

    1. REDIS 관련 처리 진행

--- src/main/java/com/project/springchatapi2/controller/ChatController.java
+++ src/main/java/com/project/springchatapi2/controller/ChatController.java
@@ -1,6 +1,8 @@
 package com.project.springchatapi2.controller;
 
 import com.project.springchatapi2.model.ChatMessage;
+import com.project.springchatapi2.repo.ChatRoomRepository;
+import com.project.springchatapi2.service.RedisPublisher;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
@@ -11,12 +13,17 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class ChatController {
 
-    private final SimpMessageSendingOperations messagingTemplate;
+    //private final SimpMessageSendingOperations messagingTemplate;
+    private final RedisPublisher redisPublisher;
+    private final ChatRoomRepository chatRoomRepository;
 
     @MessageMapping("/chat/message")
     public void message(ChatMessage message) {
-        if (ChatMessage.MessageType.ENTER.equals(message.getType()))
+        if (ChatMessage.MessageType.ENTER.equals(message.getType())){
+            chatRoomRepository.enterChatRoom(message.getRoomId());
             message.setMessage(message.getSender() + "님이 입장하셨습니다.");
-        messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
+        }
+
+        redisPublisher.publish(chatRoomRepository.getTopic(message.getRoomId()),message);
     }
 }
